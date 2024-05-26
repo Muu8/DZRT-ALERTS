c0ff28050fb051d947a72bf7ff0f8b84e5459796
Muhannad <nfu@hotmail.com>
2024-05-26 17:10:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,16 +6,16 @@ from telegram import Bot
 import asyncio
 
 # إعداد القيم الأساسية من البيئة المحيطة
-bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
-chat_id = os.getenv('TELEGRAM_CHAT_ID')
+bot_token = 'YOUR_BOT_TOKEN'
+chat_id = 'YOUR_CHAT_ID'
 
 if not bot_token or not chat_id:
     raise ValueError("Please set TELEGRAM_BOT_TOKEN and TELEGRAM_CHAT_ID environment variables")
 
 bot = Bot(bot_token)
 
-initial_check_interval = 0.5  # تقليل الفاصل الزمني للتأكد من توفر المنتجات بشكل أسرع
-extended_check_interval = 0.5  # زيادة الفاصل الزمني عند العثور على توفر المنتجات لتجنب الطلبات المتكررة
+initial_check_interval = 1  # تقليل الفاصل الزمني للتأكد من توفر المنتجات بشكل أسرع
+extended_check_interval = 5  # زيادة الفاصل الزمني عند العثور على توفر المنتجات لتجنب الطلبات المتكررة
 product_url = "https://www.dzrt.com/ar/our-products.html"
 
 last_availability = {}
@@ -55,7 +55,6 @@ async def check_product_availability(session, url):
             availability = "unavailable" not in item["class"]
             
             if product_name not in last_availability or last_availability[product_name] != availability:
-                last_availability[product_name] = availability
                 if availability:
                     print(f"Product '{product_name}' is now available at position {index}.")
                     try:
@@ -64,6 +63,11 @@ async def check_product_availability(session, url):
                         print(f"Failed to send message: {e}")
                 else:
                     print(f"Product '{product_name}' is now unavailable at position {index}.")
+                    try:
+                        await bot.send_message(chat_id=chat_id, text=f' {product_name} نفذت كميتها الآن')
+                    except Exception as e:
+                        print(f"Failed to send message: {e}")
+                last_availability[product_name] = availability
             else:
                 print(f"Product '{product_name}' availability unchanged.")
         else:
