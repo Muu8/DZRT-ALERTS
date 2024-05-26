7269c86b860541a66e976e1baaa9658c753cd173
Muhannad <nfu@hotmail.com>
2024-05-26 17:03:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,8 +14,8 @@ if not bot_token or not chat_id:
 
 bot = Bot(bot_token)
 
-initial_check_interval = 0.10  # تقليل الفاصل الزمني للتأكد من توفر المنتجات بشكل أسرع
-extended_check_interval = 0.10  # زيادة الفاصل الزمني عند العثور على توفر المنتجات لتجنب الطلبات المتكررة
+initial_check_interval = 0.5  # تقليل الفاصل الزمني للتأكد من توفر المنتجات بشكل أسرع
+extended_check_interval = 0.5  # زيادة الفاصل الزمني عند العثور على توفر المنتجات لتجنب الطلبات المتكررة
 product_url = "https://www.dzrt.com/ar/our-products.html"
 
 last_availability = {}
