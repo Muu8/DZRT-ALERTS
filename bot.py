11bc6a39f90fe82c7e3387601ad35e393a5adc21
Muhannad <nfu@hotmail.com>
2024-06-09 07:27:55 +0300

    Remove print check bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ if not bot_token or not chat_id:
 bot = Bot(bot_token)
 
 initial_check_interval = 0.1  # تقليل الفاصل الزمني للتأكد من توفر المنتجات بشكل أسرع
-extended_check_interval = 0.1  # زيادة الفاصل الزمني عند العثور على توفر المنتجات لتجنب الطلبات المتكررة
+extended_check_interval = 0  # زيادة الفاصل الزمني عند العثور على توفر المنتجات لتجنب الطلبات المتكررة
 product_url = "https://www.dzrt.com/ar/our-products.html"
 
 last_availability = {}
@@ -37,14 +37,12 @@ async def check_product_availability(session, url):
         async with session.get(url, headers=headers) as response:
             response.raise_for_status()
             html = await response.text()
-            print("Fetched page successfully")  # رسالة تشخيصية
     except aiohttp.ClientError as e:
         print(f"Failed to fetch the page: {e}")
         return
     
     soup = BeautifulSoup(html, 'html.parser')
     product_items = soup.select("li.item.product.product-item")
-    print(f"Found {len(product_items)} products on the page")  # رسالة تشخيصية
     
     for index, item in enumerate(product_items):
         product_name_tag = item.find("a", {"class": "product-item-link"})
@@ -59,7 +57,6 @@ async def check_product_availability(session, url):
             
             if last_availability[product_name] != availability:
                 if availability:
-                    print(f"Product '{product_name}' is now available at position {index}.")
                     try:
                         await bot.send_message(
                             chat_id=chat_id,
@@ -69,16 +66,11 @@ async def check_product_availability(session, url):
                     except Exception as e:
                         print(f"Failed to send message: {e}")
                 else:
-                    print(f"Product '{product_name}' is now unavailable at position {index}.")
                     try:
                         await bot.send_message(chat_id=chat_id, text=f' {product_name} نفذت الكمية ❌ \n')
                     except Exception as e:
                         print(f"Failed to send message: {e}")
                 last_availability[product_name] = availability
-            else:
-                print(f"Product '{product_name}' availability unchanged.")
-        else:
-            print(f"Product name or link not found for item at position {index}.")
 
 async def main():
     async with aiohttp.ClientSession() as session:
